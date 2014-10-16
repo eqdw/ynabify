818ddde3427d1c161e9ebf7b53a12e7304765479
Tim Herd <rz@eqdw.net>
2014-10-15 20:29:10 -0700

    Validations and convenience methods on convert command

--- lib/ynabify/commands/convert.rb
+++ lib/ynabify/commands/convert.rb
@@ -20,6 +20,76 @@ the user to create a rewrite rule for any row that does not
 already have one
         TEXT
       end
+
+
+      def execute
+        if validate
+
+        end
+      end
+
+
+      def validate
+        valid = true
+
+        if @params.length != 0 && @params.length != 2
+          valid = false
+          print_arg_num_error
+        end
+
+        if @flags['i'] == true || @flags["-infile"] == true
+          valid = false
+          print_flag_needs_value(:infile)
+        end
+        
+        if @flags['o'] == true || @flags["-outfile"] == true
+          valid = false
+          print_flag_needs_value(:outfile)
+        end
+
+        valid
+      end
+
+      def print_arg_num_error
+        puts <<-TEXT
+Invalid invocation. Must provide either zero or two filenames,
+or use flags.
+        TEXT
+      end
+
+      def print_flag_needs_value(in_or_out)
+        puts <<-TEXT
+Invalid invocation. Must provide filename for #{in_or_out.to_s} flag
+        TEXT
+      end
+
+      def infile
+        if @params.length == 2
+          @params[0]
+        elsif @flags['i']
+          @flags['i']
+        elsif @flags['-infile']
+          @flags['-infile']
+        else
+          :stdin
+        end
+      end
+
+      def outfile
+        if @params.length == 2
+          @params[1]
+        elsif @flags['o']
+          @flags['o']
+        elsif @flags['-outfile']
+          @flags['-outfile']
+        else
+          :stdout
+        end
+      end
+
+      def interactive?
+        @flags["I"] || @flags["-interactive"]
+      end
     end
   end
 end
--- spec/ynabify/commands/convert_spec.rb
+++ spec/ynabify/commands/convert_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Ynabify::Commands::Convert do
+  let( :argv    ) { [] }
+  let( :command ) { described_class.new(argv) }
+
   context "self" do
     context ".help" do
       it "should return usage information for the help command" do
@@ -22,6 +25,134 @@ the user to create a rewrite rule for any row that does not
 already have one
         TEXT
       end
+    end
+  end
+
+  context "#execute" do
+    context "valid" do
+      before(:each) do
+        expect(command).to receive(:validate).and_return(true)
+      end
+
+      it "should instantiate a converter"
+    end
+
+    context "invalid" do
+      before(:each) do
+        expect(command).to receive(:validate).and_return(false)
+      end
+
+      it "should not instantiate a converter"
+    end
+  end
+
+  context "#validate" do
+    context "valid" do
+      it "should return true" do
+        expect(command.validate).to be true
+      end
+    end
+
+    context "wrong number of params" do
+      let(:argv) { ["one_file.csv"] }
+
+      it "should return false" do
+        expect(command.validate).to be false
+      end
+
+      it "should call print_arg_num_error" do
+        expect(command).to receive(:print_arg_num_error)
+        command.validate
+      end
+    end
+
+    context "flag missing value" do
+      context "missing infile" do
+        let(:argv) { ["-i", "-o", "outfile.csv"] }
+
+        it "should return false" do
+          expect(command.validate).to be false
+        end
+
+        it "should print flag value error for infile" do
+          expect(command).to receive(:print_flag_needs_value).with(:infile)
+          command.validate
+        end
+      end
+
+      context "missing outfile" do
+        let(:argv) { ["-i", "infile.csv", "-o"] }
+
+        it "should return false" do
+          expect(command.validate).to be false
+        end
+
+        it "should print flag value error for outfile" do
+          expect(command).to receive(:print_flag_needs_value).with(:outfile)
+          command.validate
+        end
+      end
+    end
+  end
+
+  context "#infile" do
+    # I don't like this spec but didn't want to have to copy
+    # the setup code like twelve times
+    it "should return the infile name, following priority rules" do
+      param_command = described_class.new( 
+                       ["p_infile", "p_outfile", "-i", "s_infile", "--infile", "l_infile"] )
+
+      expect(param_command.infile).to eq("p_infile")
+
+      short_command = described_class.new(
+        ["-i", "s_infile", "--infile", "l_infile"])
+
+      expect(short_command.infile).to eq("s_infile")
+
+      long_command = described_class.new(
+        ["--infile", "l_infile"])
+
+      expect(long_command.infile).to eq('l_infile')
+    end
+
+    it "should return :stdin if no infile given" do
+      expect(command.infile).to eq(:stdin)
+    end
+  end
+
+  context "#outfile" do
+    # I don't like this spec but didn't want to have to copy
+    # the setup code like twelve times
+    it "should return the outfile name, following priority rules" do
+      param_command = described_class.new( 
+                       ["p_infile", "p_outfile", "-o", "s_outfile", "--outfile", "l_outfile"] )
+
+      expect(param_command.outfile).to eq("p_outfile")
+
+      short_command = described_class.new(
+        ["-o", "s_outfile", "--outfile", "l_outfile"])
+
+      expect(short_command.outfile).to eq("s_outfile")
+
+      long_command = described_class.new(
+        ["--outfile", "l_outfile"])
+
+      expect(long_command.outfile).to eq('l_outfile')
+    end
+
+    it "should return :stdout if no outfile given" do
+      expect(command.outfile).to eq(:stdout)
+    end
+  end
+
+  context "#interactive?" do
+    it "should return true if -I or --interactive flags present" do
+      expect( described_class.new(["-I"]).interactive? ).to be true
+      expect( described_class.new(["--interactive"]).interactive? ).to be true
+    end
+
+    it "should return false if both flags are absent" do
+      expect(command.interactive?).not_to be true
     end
   end
 end
