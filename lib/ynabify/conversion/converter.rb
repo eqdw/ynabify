233489e40f75fc75d8ea406f231f4bc18f3ca1dd
Tim Herd <rz@eqdw.net>
2014-11-09 11:00:28 -0800

    file parsing pt 0

--- lib/ynabify/conversion/converter.rb
+++ lib/ynabify/conversion/converter.rb
@@ -6,10 +6,33 @@ module Ynabify
       end
 
       def initialize(infile, outfile, interactive, mapping)
-        @infile       = infile
-        @outfile      = outfile
-        @interactive  = interactive
-        @mapping_file = mapping 
+        @infile      = infile
+        @outfile     = outfile
+        @interactive = interactive
+        @mapping     = mapping
+      end
+
+
+      def convert
+        init_objects #open files here, not in new
+      end
+
+
+      def make_mapping(mapping)
+        if mapping == :default
+          input_columns  = parse_headers!( @infile  )
+          output_columns = parse_headers!( @outfile )
+          Mapping.new(input_columns, output_columns)
+        else
+          Mapping.init_from_file(mapping)
+        end
+      end
+
+      # Parses out the first row
+      def parse_headers!(csv_file)
+        csv = CSV.open(csv_file, :headers => true)
+        csv.gets
+        csv.headers
       end
     end
   end
--- spec/ynabify/conversion/converter_spec.rb
+++ spec/ynabify/conversion/converter_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'csv'
 
 describe Ynabify::Conversion::Converter do
   let( :infile      ) { :stdin   }
@@ -25,6 +26,61 @@ describe Ynabify::Conversion::Converter do
 
         described_class.convert(infile, outfile, interactive, mapping)
       end
+    end
+  end
+
+  context "#make_mapping" do
+    let( :converter ) { Ynabify::Conversion::Converter.new(infile, outfile, interactive, mapping) }
+
+    context "by default" do
+      let( :parsed_input_headers  ) { %w( This Is Some Input  ) }
+      let( :parsed_output_headers ) { %w( This Is Some Output ) }
+
+      before(:each) do
+        expect(converter).to receive(:parse_headers!).with( infile  ).and_return( parsed_input_headers  )
+        expect(converter).to receive(:parse_headers!).with( outfile ).and_return( parsed_output_headers )
+      end
+
+      it "should parse_headers from the infile and outfile" do
+        # The method stubs in the before() are this test
+        converter.make_mapping(mapping)
+      end
+
+      it "should create a new mapping from the parsed headers" do
+        # note we stubbed the return value of parse_headers to be these
+        expect(Ynabify::Conversion::Mapping).to receive(:new).with( parsed_input_headers, parsed_output_headers)
+        converter.make_mapping(mapping)
+      end
+    end
+
+    context "with a filename" do
+      let( :mapping ) { "sample.yml" }
+
+      it "should call Mapping.init_from_file with the filename" do
+        expect(Ynabify::Conversion::Mapping).to receive(:init_from_file).with(mapping)
+
+        converter.make_mapping(mapping)
+      end
+    end
+  end
+
+  context "#parse_headers!" do
+    let( :csv_header_text ) { <<-CSV }
+This, Is, A, Header, Row
+this, is, a, data, row
+this, is, more, data, row
+    CSV
+
+    let( :converter ) { Ynabify::Conversion::Converter.new(infile, outfile, interactive, mapping) }
+
+    before(:each) do
+      parsed_csv = double('csv', :gets => nil, :headers => %w(This Is A Header Row))
+      expect(CSV).to receive(:open).and_return(parsed_csv)
+    end
+
+    it "should return a list of parsed headers " do
+      expect(converter.parse_headers!(infile)).
+        to eq( %w(This Is A Header Row) )
     end
   end
 end
